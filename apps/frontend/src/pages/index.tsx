40ba4d01e34c3fc6ff67210625f828c1e2ac3aea
Rainer5259 <rainer_tribosk8@hotmail.com>
2023-04-18 13:13:33 -0300

    refactor: Folder schemes for better scalability

---
+++ apps/frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/Home.module.css'
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div>
+          <h1>Primeiro NextJS</h1>
+        </div>
+      </main>
+    </>
+  )
+}
