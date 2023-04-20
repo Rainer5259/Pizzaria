2601fd5b6cf0384e2b933287fe52c0a8086377cb
Rainer5259 <rainer_tribosk8@hotmail.com>
2023-04-20 16:14:38 -0300

    feat: adds auth context to main component.

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -4,7 +4,20 @@ import styles from '../styles/Home.module.scss'
 import Image from 'next/image'
 import { Input } from '../components/ui/Input'
 import { Button } from '../components/ui/Button'
+import { AuthContext } from '../contexts/AuthContext'
+import { FormEvent, useContext } from 'react'
+
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault()
+    let data = {
+      email: 'RCpizzaria@email.com',
+      password: '123'
+    }
+    await signIn(data)
+  }
   return (
     <>
       <Head>
@@ -13,19 +26,23 @@ export default function Home() {
       <div className={styles.container}>
         <Image src={logo} alt="RC Pizzaria" />
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input type="text" placeholder="login" className={styles.input} />
             <Input
               type="password"
               placeholder="senha"
               className={styles.input}
             />
-          </form>
-          <div>
-            <Button type="submit" className={styles.button} loading={false}>
+
+            <Button
+              type="submit"
+              className={styles.button}
+              loading={false}
+              // onClick={() => signIn()}
+            >
               Acessar
             </Button>
-          </div>
+          </form>
           <a className={styles.text} href="http://localhost:3000/register?">
             Registrar Empresa
           </a>
