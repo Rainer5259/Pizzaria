d4170f3de5ee4fe983cf7c5e18b5d13870abce8d
Rainer5259 <rainer_tribosk8@hotmail.com>
2023-04-20 14:04:49 -0300

    feat: new page - create new product

---
+++ apps/frontend/src/pages/createNewProduct.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import styles from '../styles/CreateNewProduct.module.scss'
+import { Header } from '../components/comuns/Header'
+import { Button } from '../components/ui/Button'
+import { Input } from '../components/ui/Input'
+export default function createNewProduct() {
+  return (
+    <>
+      <Head>
+        <title>Orders</title>
+      </Head>
+      <Header />
+
+      <div className={styles.container}>
+        <div className={styles.newProductContainer}>
+          <form>
+            <h1>Novo Produto</h1>
+            <select
+              name="select"
+              placeholder="a"
+              className={styles.selectProduct}
+            >
+              <option disabled={true} selected={true}>
+                Selecione o produto
+              </option>
+              <option>Pizza</option>
+              <option>Refri</option>
+              <option>Aperitivos</option>
+            </select>
+
+            <Input
+              type="text"
+              className={styles.input}
+              placeholder="Nome do item"
+            />
+
+            <Input type="text" className={styles.input} placeholder="Valor" />
+
+            <Input
+              type="text"
+              className={styles.description}
+              placeholder="Descrição"
+            />
+
+            <Button type="submit" className={styles.button} loading={true}>
+              Cadastrar
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
