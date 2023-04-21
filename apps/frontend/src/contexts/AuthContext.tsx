a3ec50021ff685e75b07bc93bd13f8d890906ddb
Rainer5259 <rainer_tribosk8@hotmail.com>
2023-04-21 12:41:24 -0300

    feat: adds authentication settings to the API

--- apps/frontend/src/contexts/AuthContext.tsx
+++ apps/frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,12 @@
+import Router from 'next/router'
+import { destroyCookie } from 'nookies'
 import { createContext, ReactNode, useState } from 'react'
 
 type AuthContextData = {
   user?: UserProps
   isAuthenticated: boolean
   signIn: (credentials: SignInProps) => Promise<void>
+  signOut: () => void
 }
 type UserProps = {
   id: string
@@ -19,6 +22,15 @@ type AuthProviderProps = {
 }
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut() {
+  try {
+    destroyCookie(undefined, '@nextauth.token')
+    Router.push('/')
+  } catch {
+    console.log('Erro ao deslogar')
+  }
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>()
   const isAuthenticated = !!user
@@ -28,7 +40,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   )
