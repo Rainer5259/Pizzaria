3c96361b8ad13502b4253246125d9a625b748a99
Rainer5259 <rainer_tribosk8@hotmail.com>
2023-05-08 15:51:28 -0300

    fix: param received is undefined

--- apps/backend/src/controllers/order/DetailOrderController.ts
+++ apps/backend/src/controllers/order/DetailOrderController.ts
@@ -4,13 +4,15 @@ import { ERROR } from '../../constants/httpStatusCode/index.json'
 class DetailOrderController {
   async handle(req: Request, res: Response) {
     try {
-      const order_id = req.body.order_id as string
+      const order_id = req.query.order_id as string
+
       const detailOrderService = new DetailOrderService()
 
-      const orders = await detailOrderService.execute({ order_id })
+      const orders = await detailOrderService.execute({
+        order_id
+      })
       return res.json(orders)
     } catch (e) {
-      throw new Error(ERROR.UNPROCESSABLE_ENTITY.MESSAGE)
       return res
         .status(ERROR.UNPROCESSABLE_ENTITY.CODE)
         .json({ error: ERROR.UNPROCESSABLE_ENTITY.MESSAGE })
