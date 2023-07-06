fdadc11bfaa43f02bcae7335c5de420d6621b3fd
Rainer <rainer_tribosk8@hotmail.com>
2023-07-06 01:15:46 -0300

    refactor: update multer file upload configuration

--- apps/backend/src/config/multer.ts
+++ apps/backend/src/config/multer.ts
@@ -1,6 +1,5 @@
 import crypto from 'crypto'
-import multer from "multer";
-
+import multer from 'multer'
 import { extname, resolve } from 'path'
 
 export default {
@@ -9,13 +8,12 @@ export default {
       storage: multer.diskStorage({
         destination: resolve(__dirname, '..', '..', folder),
         filename: (request, file, callback) => {
-          const fileHash = crypto.randomBytes(16).toString("hex");
-          const fileName = `${fileHash}-${file.originalname}`
-
+          const fileHash = crypto.randomBytes(8).toString('hex')
+          const fileExtension = extname(file.originalname)
+          const fileName = `${fileHash}${fileExtension}`
           return callback(null, fileName)
         }
       })
     }
-
   }
-} 
+}
