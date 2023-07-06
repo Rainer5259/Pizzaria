f0bf1674f85890f5ed4eb49f4c2b07009bec929c
Rainer <rainer_tribosk8@hotmail.com>
2023-07-06 00:24:49 -0300

    feat: Configure Content-Security-Policy header

--- apps/backend/src/server/server.ts
+++ apps/backend/src/server/server.ts
@@ -6,16 +6,30 @@ import path from 'path'
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "script-src 'self', style-src 'self'; img-src 'self' data:; font-src 'self'"
+  )
+  next()
+})
+
 app.use(express.json())
 
 app.use(cors())
+
+// app.get('/images/product/:filename', (req, res) => {
+//   const { filename } = req.params
+//   const imagePath = path.resolve(__dirname, '../../tmp', filename)
+
+//   res.sendFile(imagePath)
+// })
 
 app.use(router)
 
 app.use('/files', express.static(path.resolve(__dirname, '..', '..', 'tmp')))
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  //case instance of type Error, do...
   if (err instanceof Error) {
     return res.status(400).json({ error: err.message })
   }
