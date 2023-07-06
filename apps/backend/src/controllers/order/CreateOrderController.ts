336af75ea56327fb16e560c018d9327c5694d124
Rainer <rainer_tribosk8@hotmail.com>
2023-07-06 00:27:22 -0300

    refactor: Add error handling using try-catch

--- apps/backend/src/controllers/order/CreateOrderController.ts
+++ apps/backend/src/controllers/order/CreateOrderController.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express'
 import { CreateOrderService } from '../../services/order/CreateOrderService'
-
+import { ERROR } from '../../constants/httpStatusCode/index.json'
 class CreateOrderController {
   async handle(req: Request, res: Response) {
-    const { table, name } = req.body
+    try {
+      const { table, name } = req.body
 
-    const createOrderService = new CreateOrderService()
+      const createOrderService = new CreateOrderService()
 
-    const order = await createOrderService.execute({ table, name })
+      const order = await createOrderService.execute({ table, name })
 
-    return res.json(order)
+      return res.json(order)
+    } catch (e) {
+      return res
+        .status(ERROR.UNPROCESSABLE_ENTITY.CODE)
+        .json({ error: ERROR.UNPROCESSABLE_ENTITY.MESSAGE })
+    }
   }
 }
 export { CreateOrderController }
--- apps/backend/src/services/order/CreateOrderService.ts
+++ apps/backend/src/services/order/CreateOrderService.ts
@@ -1,18 +1,22 @@
 import prismaClient from '../../prisma'
-
+import { ERROR } from '../../constants/httpStatusCode/index.json'
 interface OrderRequest {
   table: number
   name: string
 }
 class CreateOrderService {
   async execute({ table, name }: OrderRequest) {
-    const order = await prismaClient.order.create({
-      data: {
-        table: table,
-        name: name
-      }
-    })
-    return order
+    try {
+      const order = await prismaClient.order.create({
+        data: {
+          table: table,
+          name: name
+        }
+      })
+      return order
+    } catch (e) {
+      throw new Error(ERROR.UNAUTHORIZED.MESSAGE || e.message)
+    }
   }
 }
 export { CreateOrderService }
